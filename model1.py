ef61b50cefb5c5f3427f3e20b47b62719e44be41
Thomas <awtpianka@gmail.com>
2024-04-21 21:51:58 -0400

    Complete model 1

--- model1.py
+++ model1.py
@@ -2,11 +2,11 @@
 import pandas
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as func
 import torch.optim as optim
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from torch.autograd import Variable
 from torch.utils.data import Dataset
 
@@ -14,13 +14,12 @@ args = {}
 kwargs = {}
 args['batch_size'] = 64
 args['test_batch_size'] = 64
-args['epochs'] = 15
-args['lr'] = 0.1
-args['momentum'] = 0.6
+args['epochs'] = 3
+args['lr'] = 0.01
+args['momentum'] = 0.5
 args['seed'] = 1
 args['log_interval'] = 10
-args['cuda'] = True
-device = torch.device('cuda')
+args['cuda'] = False
 
 
 class PhishingDataset(Dataset):
@@ -37,24 +36,9 @@ class PhishingDataset(Dataset):
 
     def __getitem__(self, idx):
         sample = torch.tensor(self.data.iloc[idx, :-1], dtype=torch.float32)
-        target = torch.tensor(self.data.iloc[idx, -1], dtype=torch.long)
+        target = torch.tensor(self.data.iloc[idx, -1], dtype=torch.float32)
 
         return sample, target
-
-
-csv_file = 'PhiUSIIL_Phishing_URL_Dataset.csv'
-dataset = PhishingDataset(csv_file)
-
-training_X, test_X = train_test_split(dataset.data.iloc[:, :-1], test_size=0.2, random_state=args['seed'])
-training_Y, test_Y = train_test_split(dataset.data.iloc[:, -1], test_size=0.2, random_state=args['seed'])
-
-training_set = torch.utils.data.TensorDataset(torch.tensor(training_X.values, dtype=torch.float32),
-                                              torch.tensor(training_Y.values, dtype=torch.float32))
-test_set = torch.utils.data.TensorDataset(torch.tensor(test_X.values, dtype=torch.long),
-                                          torch.tensor(test_Y.values, dtype=torch.long))
-
-train_loader = torch.utils.data.DataLoader(training_set, batch_size=args['batch_size'], shuffle=True, **kwargs)
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=args['test_batch_size'], shuffle=True, **kwargs)
 
 
 class Net(nn.Module):
@@ -62,26 +46,50 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(54, 128)
         self.fc2 = nn.Linear(128, 32)
-        self.fc3 = nn.Linear(32, 2)
+        self.fc3 = nn.Linear(32, 1)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x.float()))
-        x = F.relu(self.fc2(x))
+        x = func.relu(self.fc1(x))
+        x = func.relu(self.fc2(x))
         x = self.fc3(x)
 
-        return F.softmax(x, dim=1)
+        return x
+
+
+csv_file = 'PhiUSIIL_Phishing_URL_Dataset.csv'
+dataset = PhishingDataset(csv_file)
+
+training_X, test_X, training_Y, test_Y = train_test_split(dataset.data.iloc[:, :-1], dataset.data.iloc[:, -1],
+                                                          test_size=0.2, random_state=args['seed'])
+
+scaler = MinMaxScaler()
+training_X = scaler.fit_transform(training_X.values)
+test_X = scaler.fit_transform(test_X.values)
+
+training_tensor_X = torch.from_numpy(training_X).float()
+training_tensor_Y = torch.from_numpy(training_Y.values.ravel()).float()
+test_tensor_X = torch.from_numpy(test_X).float()
+test_tensor_Y = torch.from_numpy(test_Y.values.ravel()).float()
+
+training_tensor_Y = training_tensor_Y.unsqueeze(1)
+test_tensor_Y = test_tensor_Y.unsqueeze(1)
+
+training_set = torch.utils.data.TensorDataset(training_tensor_X, training_tensor_Y)
+test_set = torch.utils.data.TensorDataset(test_tensor_X, test_tensor_Y)
+
+train_loader = torch.utils.data.DataLoader(training_set, batch_size=args['batch_size'], shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=args['test_batch_size'], shuffle=True, **kwargs)
 
 
 def train(epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
-        target = target.type(torch.LongTensor)
         if args['cuda']:
             data, target = data.cuda(), target.cuda()
         data, target = Variable(data), Variable(target)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.cross_entropy(output, target)
+        loss = func.binary_cross_entropy_with_logits(output, target)
         loss.backward()
         optimizer.step()
         if batch_idx % args['log_interval'] == 0:
@@ -101,9 +109,11 @@ def test():
                 data, target = data.cuda(), target.cuda()
             data, target = Variable(data), Variable(target)
             output = model(data)
-            test_loss += F.nll_loss(output, target, size_average=False).data.item()
-            pred = output.data.max(1, keepdim=True)[1]
-            correct += pred.eq(target.data.view_as(pred)).long().cpu().sum()
+            test_loss += func.binary_cross_entropy_with_logits(output, target).data.item()
+            target = target.squeeze()
+            output = output.squeeze()
+            output = output > .5
+            correct += (target.data == output.data).sum().item()
 
     test_loss /= len(test_loader.dataset)
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
